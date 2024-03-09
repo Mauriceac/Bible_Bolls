6362731474a611367efcf0b94bb71789b9a4c637
Mauriceac <maurice.alvarado@teck.com>
2024-03-09 03:48:38 -0300

    code refactored and optimized

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,4 +1,5 @@
 // Gospel reading of the day
+
 document.getElementById("today").addEventListener("click", async function () {
   // Add async keyword here
 
@@ -10,74 +11,55 @@ document.getElementById("today").addEventListener("click", async function () {
 
   // Split the gospel string into book, chapter, and verses
   var parts = gospel.split(" ");
-  var bookName = parts[0];
+  let bookName = parts[0];
+  switch (bookName) {
+    case "Matthew":
+      bookId = 40;
+      break;
+    case "Mark":
+      bookId = 41;
+      break;
+    case "Luke":
+      bookId = 42;
+      break;
+    case "John":
+      bookId = 43;
+      break;
+    default:
+      console.log("Book not found");
+      break;
+  }
+
   var chapterAndVerses = parts[1].split(".");
   var chapter = chapterAndVerses[0];
-  var verses = chapterAndVerses[1].split("-");
-
-  document.getElementById("todaysGospel").innerHTML = gospel;
-  console.log(
-    "Today's Gospel: " +
-      bookName +
-      " " +
-      chapter +
-      ":" +
-      verses[0] +
-      "-" +
-      verses[1]
-  );
-
-  document.getElementById("translationList").value = "NJB1985";
-  var event = new Event("change");
-  document.getElementById("translationList").dispatchEvent(event);
-
-  // Get the bookList select element
-
-  var bookList = document.getElementById("bookList");
-
-  // Wait for the options to be filled
-  while (bookList.options.length === 0) {
-    await new Promise((resolve) => setTimeout(resolve, 500)); // Wait for 100ms before checking again
-
-    // Find the option with the text that matches the bookName
-    var option = Array.from(bookList.options).find(
-      (option) => option.text === bookName
-    );
-
-    // If the option was found, set the value of the bookList select element to the value of the option
-    if (option) {
-      bookList.value = option.value;
-      bookList.dispatchEvent(event);
-    }
+  var match = chapterAndVerses[1].match(/\d+/);
+  let verse;
+  if (match) {
+    verse = parseInt(match[0], 10);
   }
 
-  while (chapterList.options.length === 0) {
-    await new Promise((resolve) => setTimeout(resolve, 500)); // Wait for 100ms before checking again
-
-    // Find the option with the text that matches the bookName
-    var option = Array.from(chapterList.options).find(
-      (option) => option.value === chapter
-    );
-
-    // If the option was found, set the value of the bookList select element to the value of the option
-    if (option) {
-      chapterList.value = option.value;
-      chapterList.dispatchEvent(event);
+  if (
+    document.getElementById("translationList").value === "Select a translation"
+  ) {
+    alert("Please select a translation first.");
+  } else if (books.find((book) => book.bookid == bookId) === undefined) {
+    alert("Please select a translation that includes the New Testament.");
+  } else {
+    // Display the gospel citation
+    document.getElementById("todaysGospel").innerHTML = gospel;
+    // Get the passage
+    async function setOptions(menu, value) {
+      var menu = document.getElementById(menu);
+      while (menu.length <= 1) {
+        await new Promise((r) => setTimeout(r, 5000));
+      }
+      menu.value = value;
+      menu.dispatchEvent(new Event("change"));
     }
+
+    setOptions("bookList", bookId);
+    setOptions("chapterList", chapter);
+    setOptions("verseList", verse);
   }
 
-  while (verseList.options.length === 0) {
-    await new Promise((resolve) => setTimeout(resolve, 500)); // Wait for 100ms before checking again
-    console.log("verseList: " + document.getElementById("verseList").value);
-    // Find the option with the text that matches the bookName
-    var option = Array.from(verseList.options).find(
-      (option) => option.value === verses[0]
-    );
-
-    // If the option was found, set the value of the bookList select element to the value of the option
-    if (option) {
-      verseList.value = option.value;
-      verseList.dispatchEvent(event);
-    }
-  }
 });
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,31 @@
 var dictionary = document.querySelector(".secondary-display");
 const path = "http://localhost:3000";
+let shortName;
+let bookId;
+let books;
+let chapterId;
+let chapter;
+let verseId;
+let originalShortName;
+let originalChapter;
+let originalVerse;
+let originalText;
+
+// Function to populate a dropdown menu with options
+function populateDropdown(dropdown, options) {
+  const defaultOption = document.createElement("option");
+  const list = `${dropdown.id}`.replace(/List$/, "");
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  defaultOption.textContent = `Select a ${list}`;
+  dropdown.appendChild(defaultOption);
+  options.forEach((option) => {
+    const newOption = document.createElement("option");
+    newOption.value = option.value;
+    newOption.text = option.text;
+    dropdown.appendChild(newOption);
+  });
+}
 
 // manual selection of translation, book, chapter, and verse
 document.addEventListener("DOMContentLoaded", function () {
@@ -26,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("translationList")
     .addEventListener("change", function () {
-      const shortName = this.value;
+      shortName = this.value;
       console.log(`Translation selected: ${shortName}`);
 
       // Reset bookId and clear bookList dropdown
@@ -41,7 +67,8 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((response) => response.json())
         .then((data) => {
           const options = [];
-          data.forEach((book) => {
+          books = data;
+          books.forEach((book) => {
             options.push({ value: book.bookid, text: book.name });
           });
           populateDropdown(bookList, options);
@@ -51,41 +78,46 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Event listener for book selection
 document.getElementById("bookList").addEventListener("change", function () {
-  const shortName = document.getElementById("translationList").value;
-  const bookId = this.value;
+  bookId = this.value;
+
   console.log(`Book selected: ${bookId}`);
 
+  if (bookId >= 40 && bookId <= 66) {
+    originalShortName = "TISCH";
+  }
+  if (bookId >= 1 && bookId <= 39) {
+    originalShortName = "WLCa";
+  }
+  if (bookId >= 67) {
+    originalShortName = "LXX";
+  }
+  if (shortName === "LXXE") {
+    originalShortName = "LXX";
+  }
+
   // Reset chapterList dropdown
-  const chapterList = document.getElementById("chapterList");
+  let chapterList = document.getElementById("chapterList");
   chapterList.value = "";
   while (chapterList.firstChild) {
     chapterList.removeChild(chapterList.firstChild);
   }
 
   // Fetch available books in the selected translation
-  fetch(`${path}/get-books/${shortName}/`)
-    .then((response) => response.json())
-    .then((data) => {
-      // Find the selected book
-      const selectedBook = data.find((book) => book.bookid == bookId);
-      if (selectedBook) {
-        // Create an option for each chapter in the book
-        const options = [];
-        for (let i = 1; i <= selectedBook.chapters; i++) {
-          options.push({ value: i, text: `Chapter ${i}` });
-        }
-        populateDropdown(chapterList, options);
-      }
-
-      console.log(`Chapters loaded:` + chapterList.length);
-    });
+  const selectedBook = books.find((book) => book.bookid == bookId);
+  if (selectedBook) {
+    // Create an option for each chapter in the book
+    const options = [];
+    for (let i = 1; i <= selectedBook.chapters; i++) {
+      options.push({ value: i, text: `Chapter ${i}` });
+    }
+    populateDropdown(chapterList, options);
+  }
+  console.log(`Chapters loaded:` + chapterList.length);
 });
 
 // Event listener for chapter selection
 document.getElementById("chapterList").addEventListener("change", function () {
-  const shortName = document.getElementById("translationList").value;
-  const bookId = document.getElementById("bookList").value;
-  const chapterId = this.value;
+  chapterId = this.value;
   console.log(`Chapter selected: ${chapterId}`);
 
   // Reset verseList dropdown
@@ -99,123 +131,85 @@ document.getElementById("chapterList").addEventListener("change", function () {
   fetch(`${path}/get-text/${shortName}/${bookId}/${chapterId}/`)
     .then((response) => response.json())
     .then((data) => {
+      chapter = data;
       const options = [];
-      for (let i = 1; i <= data.length; i++) {
+      for (let i = 1; i <= chapter.length; i++) {
         options.push({ value: i, text: `Verse ${i}` });
       }
       populateDropdown(verseList, options);
-
       console.log(`Verses loaded:` + verseList.length);
+    });
+
+  fetch(`${path}/get-text/${originalShortName}/${bookId}/${chapterId}/`)
+    .then((response1) => response1.json())
+    .then((data1) => {
+      originalChapter = data1;
     });
 });
 
 // Event listener for verse selection
 document
   .getElementById("verseList")
-  .addEventListener("change", async function () {
-    const shortName = document.getElementById("translationList").value;
-    const bookId = document.getElementById("bookList").value;
-    const chapterNumber = document.getElementById("chapterList").value;
-    const verseNumber = this.value;
-    console.log(`Verse selected: ${verseNumber}`);
+  .addEventListener("change", function () {
+    verseId = this.value;
+    console.log(`Verse selected: ${verseId}`);
 
-    // Fetch the selected verse and display it
-    await fetch(
-      `${path}/get-verse/${shortName}/${bookId}/${chapterNumber}/${verseNumber}/`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const verseText = document.getElementById("verseText");
-        verseText.innerHTML = data.text;
-      });
+    // Select verse and display it
+    let verseText = document.getElementById("verseText");
+    verseText.innerHTML = chapter[verseId - 1].text;
 
-    // Fetch the Greek or Hebrew text and display it. If the the bookId is equal or greater than 40 and equal or less than 66, then it is the New Testament and the Greek text is displayed. If the bookId is other than that range, then it is the Old Testament and the Hebrew text is displayed.
+    getOriginalVerse();
 
-    let originalShortName; // Declare originalShortName here
-
-    if (bookId >= 40 && bookId <= 66) {
-      originalShortName = "TISCH"; // Assign a value here
-    }
-    if (bookId >= 1 && bookId <= 39) {
-      originalShortName = "WLCa"; // Assign a value here
-    }
-    if (bookId >= 67) {
-      originalShortName = "LXX"; // Assign a value here
-    }
-
-    fetch(
-      `${path}/get-verse/${originalShortName}/${bookId}/${chapterNumber}/${verseNumber}/`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (originalShortName === "TISCH" || originalShortName === "WLCa") {
-          const verseWithStrong = data.text;
-          const strongNumbers = verseWithStrong
-            .match(/<S>(\d+)<\/S>/g)
-            .map((s) => s.replace(/<\/?S>/g, ""));
-          const words = verseWithStrong
-            .split(/<S>\d+<\/S>/g)
-            .filter((s) => s.length > 0);
-          const wordsWithSpans = words
-            .filter((s) => s.trim().length > 0)
-            .map((word, i) => {
-              const strongNumber = strongNumbers[i];
-              if (originalShortName === "TISCH") {
-                return `<span id="G${strongNumber}">${word.trim()}</span>`;
-              }
-              if (originalShortName === "WLCa") {
-                return `<span id="H${strongNumber}">${word.trim()}</span>`;
-              }
-            });
-          const result = wordsWithSpans.map((word, i) => ({
-            word,
-            strongNumber: strongNumbers[i],
-          }));
-          const originalText = document.getElementById("originalText");
-          originalText.innerHTML = wordsWithSpans.join(" ");
-          console.log(result);
-
-          // add event listeners to each word and display the strong number when clicked
-          const verseWords = document.querySelectorAll("span");
-          verseWords.forEach((word) => {
-            word.addEventListener("click", function () {
-              if (dictionary.classList.contains("open")) {
-                dictionary.classList.replace("open", "close");
-              }
-              const definition = document.getElementById("definition");
-              const wordVariant = document.getElementById("wordVariant");
-              const strongNumber = this.id;
-              console.log(`Strong number clicked: ${strongNumber}`);
-              fetch(`${path}/dictionary-definition/BDBT/${strongNumber}/`)
-                .then((response) => response.json())
-                .then((data) => {
-                  console.log(data);
-
-                  wordVariant.innerHTML = word.textContent;
-                  definition.innerHTML = data[0].definition;
-
-                  dictionary.classList.replace("close", "open");
-                });
-            });
+    function getOriginalVerse() {
+      originalVerse = originalChapter[verseId - 1].text;
+      if (originalShortName === "TISCH" || originalShortName === "WLCa") {
+        const verseWithStrong = originalVerse;
+        const strongNumbers = verseWithStrong
+          .match(/<S>(\d+)<\/S>/g)
+          .map((s) => s.replace(/<\/?S>/g, ""));
+        const words = verseWithStrong
+          .split(/<S>\d+<\/S>/g)
+          .filter((s) => s.length > 0);
+        const wordsWithSpans = words
+          .filter((s) => s.trim().length > 0)
+          .map((word, i) => {
+            const strongNumber = strongNumbers[i];
+            if (originalShortName === "TISCH") {
+              return `<span id="G${strongNumber}">${word.trim()}</span>`;
+            }
+            if (originalShortName === "WLCa") {
+              return `<span id="H${strongNumber}">${word.trim()}</span>`;
+            }
           });
-        } else {
-          const originalText = document.getElementById("originalText");
-          originalText.innerHTML = data.text;
-        }
-      });
-  });
 
-function populateDropdown(dropdown, options) {
-  const defaultOption = document.createElement("option");
-  const list = `${dropdown.id}`.replace(/List$/, "");
-  defaultOption.disabled = true;
-  defaultOption.selected = true;
-  defaultOption.textContent = `Select a ${list}`;
-  dropdown.appendChild(defaultOption);
-  options.forEach((option) => {
-    const newOption = document.createElement("option");
-    newOption.value = option.value;
-    newOption.text = option.text;
-    dropdown.appendChild(newOption);
+        originalText = document.getElementById("originalText");
+        originalText.innerHTML = wordsWithSpans.join(" ");
+
+        let verseWords = document.querySelectorAll("span");
+        verseWords.forEach((word) => {
+          word.addEventListener("click", function () {
+            if (dictionary.classList.contains("open")) {
+              dictionary.classList.replace("open", "close");
+            }
+            let definition = document.getElementById("definition");
+            let wordVariant = document.getElementById("wordVariant");
+            let strongNumber = this.id;
+            console.log(`Strong number clicked: ${strongNumber}`);
+            fetch(`${path}/dictionary-definition/BDBT/${strongNumber}/`)
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data);
+
+                wordVariant.innerHTML = word.textContent;
+                definition.innerHTML = data[0].definition;
+
+                dictionary.classList.replace("close", "open");
+              });
+          });
+        });
+      } else {
+        originalText = document.getElementById("originalText");
+        originalText.innerHTML = originalChapter[verseId - 1].text;
+      }
+    }
   });
-}
